30cc36fcc42ce7f518bb3b84c5d7733a508534e9
Justin van der Hooft <jjjvanderhooft@gmail.com>
2021-04-01 10:49:47 +0200

    added functions to example.py

--- example.py
+++ example.py
@@ -4,3 +4,16 @@ temperature = 10
 
 def print_temperature():
     print(temperature)
+
+def add(a,b):
+    return a + b
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add('space', 'ship') == 'spaceship'
+
+def substract(a, b):
+    return a + b
+
+#def test_substract():
+#    assert substract(2, 3) == -1
